6a51a303ecaa6ab9edf5a8ed8a6fb430c54448f7
Khylko Andrii <hilko_a@itstep.academy>
2023-10-25 21:22:24 +0300

    Add project files.

---
+++ WfGitHubTest_PV221/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WfGitHubTest_PV221
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblData = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblData
+            // 
+            this.lblData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblData.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblData.Location = new System.Drawing.Point(109, 76);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(574, 208);
+            this.lblData.TabIndex = 0;
+            this.lblData.Text = "Hello, World";
+            this.lblData.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblData);
+            this.Name = "Form1";
+            this.Text = "Git Hub Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label lblData;
+    }
+}
